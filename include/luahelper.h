6ec55b8ede118bf698dc63bc3642cbe3be32f0be
Loic Blot <loic.blot@unix-experience.fr>
2017-04-27 23:39:08 +0200

    Lua: destructor should be virtual

--- include/luahelper.h
+++ include/luahelper.h
@@ -36,7 +36,7 @@ private:                                                                        
                                                                                                                        \
 public:                                                                                                                \
 	LuaRef##name(name *object);                                                                                    \
-	~LuaRef##name();                                                                                               \
+	virtual ~LuaRef##name();                                                                                       \
 	static void Register(lua_State *L);                                                                            \
 	static void create(lua_State *L, name *object);                                                                \
 	static LuaRef##name *checkobject(lua_State *L, int narg);                                                      \
